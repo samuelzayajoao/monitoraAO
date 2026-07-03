d408336d4ec751ea218a1c4a8e22fdb9b3e608b7
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2026-07-03 22:55:41 +0100

    feat: start NinjaExtra

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
+from .api import api
 
 
 def health(request):
@@ -10,4 +11,5 @@ def health(request):
 urlpatterns = [
 	path("admin/", admin.site.urls),
 	path("health/", health, name="health-check"),
+	path("api/v1/", api.urls),
 ]
