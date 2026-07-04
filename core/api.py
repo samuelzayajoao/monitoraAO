8e69739643f0d6ddb359575501623ab55705e557
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2026-07-04 14:06:03 +0100

    feat: custom user model

--- core/api.py
+++ core/api.py
@@ -1,9 +1,8 @@
 from ninja_extra import NinjaExtraAPI
 
 api = NinjaExtraAPI(
-	title="MonitoraAO",
-	description="API monitoring transactions.",
-	version="1.0.0",
-	urls_namespace="api-v1",
+    title="MonitoraAO",
+    description="API monitoring transactions.",
+    version="1.0.0",
+    urls_namespace="api-v1",
 )
-    
--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,46 +23,47 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 INSTALLED_EXTERNAL_APPS = ["ninja_extra"]
 
-INSTALLED_PROJECT_APPS = []
+INSTALLED_PROJECT_APPS = ["apps.auths"]
 
 INSTALLED_APPS = (
-	[
-		"django.contrib.admin",
-		"django.contrib.auth",
-		"django.contrib.contenttypes",
-		"django.contrib.sessions",
-		"django.contrib.messages",
-		"django.contrib.staticfiles",
-	]
-	+ INSTALLED_EXTERNAL_APPS
-	+ INSTALLED_PROJECT_APPS
+    [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+    ]
+    + INSTALLED_EXTERNAL_APPS
+    + INSTALLED_PROJECT_APPS
 )
 
 MIDDLEWARE = [
-	"django.middleware.security.SecurityMiddleware",
-	"django.contrib.sessions.middleware.SessionMiddleware",
-	"django.middleware.common.CommonMiddleware",
-	"django.middleware.csrf.CsrfViewMiddleware",
-	"django.contrib.auth.middleware.AuthenticationMiddleware",
-	"django.contrib.messages.middleware.MessageMiddleware",
-	"django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    #    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
-	{
-		"BACKEND": "django.template.backends.django.DjangoTemplates",
-		"DIRS": [],
-		"APP_DIRS": True,
-		"OPTIONS": {
-			"context_processors": [
-				"django.template.context_processors.request",
-				"django.contrib.auth.context_processors.auth",
-				"django.contrib.messages.context_processors.messages",
-			],
-		},
-	},
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
 
 ASGI_APPLICATION = "core.asgi.application"
@@ -71,14 +73,14 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
-	"default": {
-		"ENGINE": env("DATABASE_ENGINE", default="django.db.backends.sqlite3"),
-		"NAME": env("DATABASE_NAME", default="polls"),
-		"USER": env("DATABASE_USERNAME", default="myprojectuser"),
-		"PASSWORD": env("DATABASE_PASSWORD", default="password"),
-		"HOST": env("DATABASE_HOST", default="127.0.0.1"),
-		"PORT": env("DATABASE_PORT", default=5432),
-	}
+    "default": {
+        "ENGINE": env("DATABASE_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": env("DATABASE_NAME", default="polls"),
+        "USER": env("DATABASE_USERNAME", default="myprojectuser"),
+        "PASSWORD": env("DATABASE_PASSWORD", default="password"),
+        "HOST": env("DATABASE_HOST", default="127.0.0.1"),
+        "PORT": env("DATABASE_PORT", default=5432),
+    }
 }
 
 
@@ -86,18 +88,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-	},
-	{
-		"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-	},
-	{
-		"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-	},
-	{
-		"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-	},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -112,8 +114,18 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "auths.CustomUser"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+# The absolute path to the directory where collectstatic will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Additional locations of static files (if you have a local static folder)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
